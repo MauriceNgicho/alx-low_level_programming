2ff329549ecb85ab7d0048215c25170c080ab175
MauriceNgicho <mrcngicho@gmail.com>
2024-06-21 16:16:42 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: 'Prints "with proper grammar, but the outcome is a piece of art,"'
+ * Description: 'Prints "with proper grammar, but the outcome is an art,"'
  *
  * Return: Always 0 (Success)
  */
@@ -11,4 +11,5 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+
 }
