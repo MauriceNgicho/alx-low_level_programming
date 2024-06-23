c2790dfb27ee634835dc1e29912063fdb55698d0
MauriceNgicho <mrcngicho@gmail.com>
2024-06-23 12:15:07 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabets from a-z
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+		putchar('\n');
+
+		return (0);
+}
