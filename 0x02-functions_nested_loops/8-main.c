1969751870fbbd41bd37fc432172d6c2d64b66c2
MauriceNgicho <mrcngicho@gmail.com>
2024-06-26 18:30:26 -0700

    24_hours

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int main(void)
+{
+	int r;
+
+
+	r = print_last_digit(98);
+	_putchar(' ');
+	_putchar('=');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+
+
+	r = print_last_digit(0);
+	_putchar(' ');
+	_putchar('=');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+
+
+	r = print_last_digit(-1024);
+	_putchar(' ');
+	_putchar('=');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+
+	return (0);
+}
