23595bc8085386c1d8ce02299c97be0daff2c42c
MauriceNgicho <mrcngicho@gmail.com>
2024-06-30 05:58:53 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,10 +10,7 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
-	
-	write(2, message, 59);  /* 2 is the file descriptor for standard error */
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
