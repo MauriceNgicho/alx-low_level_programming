df20aa85ba255e12474b507f418574c840a07d91
MauriceNgicho <mrcngicho@gmail.com>
2024-07-08 23:52:23 -0700

    chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,6 +9,9 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
+
 
 
 #endif
