899c6b895a75e2cfbaec32af0142c77939ed7967
MauriceNgicho <mrcngicho@gmail.com>
2024-07-10 15:07:04 -0700

    value_x_raised_to_power_y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,7 +5,8 @@
  * @x: The base value.
  * @y: The exponent value.
  *
- * Return: The value of x raised to the power of y. If y is lower than 0, return -1.
+ * Return: The value of x raised to the power of y.
+ *If y is lower than 0, return -1.
  */
 int _pow_recursion(int x, int y)
 {
