92fe504f69cd2e64fc49d2f7675ac9f4ed83f73a
MauriceNgicho <mrcngicho@gmail.com>
2024-07-14 07:51:09 -0700

    prints_its_name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	(void)argc;/*to avoid unused warning*/
+	/*print program's name*/
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
