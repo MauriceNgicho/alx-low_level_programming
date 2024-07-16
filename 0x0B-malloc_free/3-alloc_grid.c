6a367c8971d755be0edd0909f42b78f1452f538c
MauriceNgicho <mrcngicho@gmail.com>
2024-07-16 13:32:03 -0700

    2_dimensional_array_of_int

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,11 +29,23 @@ int **alloc_grid(int width, int height)
 	/*Allocate memory for each row*/
 	for (i = 0; i < height; i++)
 	{
-		/* free previously allocated memory on failure*/
-		for (j = 0; j < i; j++)
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			/*free previous allocated memory on failure */
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		/*Initialize each element to 0 */
+		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
 		}
 	}
+
 	return (grid);
 }
