2307920465d96fa5274d1fee63e1ccf7a756c92c
MauriceNgicho <mrcngicho@gmail.com>
2024-08-08 19:17:41 -0700

    returns the number of bits you would need to flip to get from one number to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * flip_bits - Returns the number of bits to flip to get from one number to another.
+ * @n: The first number.
+ * @m: The second number.
+ *
+ * Return: The number of bits that need to be flipped.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int num = n ^ m; /* gives xor result */
+	unsigned int count = 0;
+
+	while (num)
+	{
+		count += num & 1; /* checks last significant bit */
+		num >>= 1; /* right shift to check next bit */
+	}
+
+	return (count);
+}
